29392602313aaf8c1ca4c3628eebb1f3ce02bbbf
Vladimir <awsvlad@gmail.com>
2023-07-02 13:03:51 -0400

    Configure variables

--- main.tf
+++ main.tf
@@ -1,38 +1,32 @@
-locals {
-  availability_zones  = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  public_subnet_cidr  = ["10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24"]
-  private_subnet_cidr = ["10.0.100.0/24", "10.0.101.0/24", "10.0.103.0/24"]
-}
-
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = var.vpc_cidr
 
   tags = {
-    Name = "dev"
+    Name = var.env_code
   }
 }
 
 resource "aws_subnet" "public" {
-  count = length(local.public_subnet_cidr)
+  count = length(var.public_subnet_cidr)
 
   vpc_id            = aws_vpc.main.id
-  cidr_block        = local.public_subnet_cidr[count.index]
-  availability_zone = local.availability_zones[count.index]
+  cidr_block        = var.public_subnet_cidr[count.index]
+  availability_zone = var.availability_zones[count.index]
 
   tags = {
-    Name = "public${count.index}"
+    Name = "${var.env_code}-public${count.index}"
   }
 }
 
 resource "aws_subnet" "private" {
-  count = length(local.private_subnet_cidr)
+  count = length(var.private_subnet_cidr)
 
   vpc_id            = aws_vpc.main.id
-  cidr_block        = local.private_subnet_cidr[count.index]
-  availability_zone = local.availability_zones[count.index]
+  cidr_block        = var.private_subnet_cidr[count.index]
+  availability_zone = var.availability_zones[count.index]
 
   tags = {
-    Name = "private${count.index}"
+    Name = "${var.env_code}-private${count.index}"
   }
 }
 
@@ -40,28 +34,28 @@ resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
 
   tags = {
-    Name = "main"
+    Name = var.env_code
   }
 }
 
 resource "aws_eip" "nat" {
-  count = length(local.public_subnet_cidr)
+  count = length(var.public_subnet_cidr)
   
   vpc = true
 
   tags = {
-    Name = "nat${count.index}"
+    Name = "${var.env_code}-nat${count.index}"
   }
 }
 
 resource "aws_nat_gateway" "main" {
-  count = length(local.public_subnet_cidr)
+  count = length(var.public_subnet_cidr)
 
   allocation_id = aws_eip.nat[count.index].id
   subnet_id     = aws_subnet.public[count.index].id
 
   tags = {
-    Name = "main${count.index}"
+    Name = "${var.env_code}-main${count.index}"
   } 
 }
 
@@ -74,12 +68,12 @@ resource "aws_route_table" "public" {
   }
 
   tags = {
-    Name = "public"
+    Name = "${var.env_code}-public"
   }
 }
 
 resource "aws_route_table" "private" {
-  count = length(local.private_subnet_cidr)
+  count = length(var.private_subnet_cidr)
 
   vpc_id = aws_vpc.main.id
 
@@ -90,14 +84,14 @@ resource "aws_route_table" "private" {
 }
 
 resource "aws_route_table_association" "public" {
-  count = length(local.public_subnet_cidr)
+  count = length(var.public_subnet_cidr)
   
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
 
 resource "aws_route_table_association" "private" {
-  count = length(local.private_subnet_cidr)
+  count = length(var.private_subnet_cidr)
   
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private[count.index].id
---
+++ terraform.tfvars
@@ -0,0 +1,9 @@
+env_code = "qa"
+
+availability_zones  = ["us-east-1a", "us-east-1b", "us-east-1c"]
+
+public_subnet_cidr  = ["10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24"]
+  
+private_subnet_cidr = ["10.0.100.0/24", "10.0.101.0/24", "10.0.103.0/24"]
+
+vpc_cidr = "10.0.0.0/16"
