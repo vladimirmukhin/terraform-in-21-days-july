b32533bb833e7ded52d28548a975699736222efa
Vladimir Mukhin <39741635+vladimirmukhin@users.noreply.github.com>
2023-07-08 13:56:30 -0400

    Install 2048 using user data (#9)

--- instance.tf
+++ instance.tf
@@ -27,6 +27,14 @@ resource "aws_security_group" "public" {
     cidr_blocks      = ["98.186.194.188/32"]
   }
 
+  ingress {
+    description      = "HTTP from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["98.186.194.188/32"]
+  }
+
   egress {
     from_port        = 0
     to_port          = 0
@@ -45,7 +53,8 @@ resource "aws_instance" "web" {
   subnet_id                   = aws_subnet.public[0].id
   vpc_security_group_ids      = [aws_security_group.public.id]
   key_name                    = "main"  
-  associate_public_ip_address = true    
+  associate_public_ip_address = true
+  user_data                   = file("user-data.sh") 
 
   tags = {
     Name = "${var.env_code}-public"
