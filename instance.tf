db8e25c0ba801d8f0d3141c6269eec6b97230b2f
Vladimir Mukhin <39741635+vladimirmukhin@users.noreply.github.com>
2023-07-08 12:52:44 -0400

    Add EC2 instances (#7)

---
+++ instance.tf
@@ -0,0 +1,90 @@
+data "aws_ami" "amazonlinux" {
+  most_recent = true
+
+  filter {
+    name  = "name"
+    values = ["amzn2-ami-kernel-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["137112412989"]
+}
+
+resource "aws_security_group" "public" {
+  name        = "${var.env_code}-public"
+  description = "Public security group"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["98.186.194.188/32"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.env_code}-public"
+  }
+}
+
+resource "aws_instance" "web" {
+  ami                         = data.aws_ami.amazonlinux.id
+  instance_type               = "t3.micro"
+  subnet_id                   = aws_subnet.public[0].id
+  vpc_security_group_ids      = [aws_security_group.public.id]
+  key_name                    = "main"  
+  associate_public_ip_address = true    
+
+  tags = {
+    Name = "${var.env_code}-public"
+  }
+}
+
+resource "aws_security_group" "private" {
+  name        = "${var.env_code}-private"
+  description = "Private security group"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [var.vpc_cidr]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.env_code}-private"
+  }
+}
+
+resource "aws_instance" "private" {
+  ami                         = data.aws_ami.amazonlinux.id
+  instance_type               = "t3.micro"
+  subnet_id                   = aws_subnet.private[0].id
+  vpc_security_group_ids      = [aws_security_group.private.id]
+  key_name                    = "main"
+
+  tags = {
+    Name = "${var.env_code}-private"
+  }
+}
