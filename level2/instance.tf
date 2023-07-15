f2c4cf24431881a6c22e44d00ccbc4be77daa7da
Vladimir <awsvlad@gmail.com>
2023-07-15 12:56:13 -0400

    Configure load balancer

--- level2/instance.tf
+++ level2/instance.tf
@@ -2,7 +2,7 @@ data "aws_ami" "amazonlinux" {
   most_recent = true
 
   filter {
-    name  = "name"
+    name   = "name"
     values = ["amzn2-ami-kernel-*"]
   }
 
@@ -16,30 +16,22 @@ data "aws_ami" "amazonlinux" {
 
 resource "aws_security_group" "public" {
   name        = "${var.env_code}-public"
-  description = "Public security group"
+  description = "allows public traffic"
   vpc_id      = data.terraform_remote_state.level1.outputs.vpc_id
 
   ingress {
-    description      = "SSH from VPC"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["98.186.198.189/32"]
-  }
-
-  ingress {
-    description      = "HTTP from VPC"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "SHH from home office"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["98.186.192.216/32"]
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
@@ -47,16 +39,13 @@ resource "aws_security_group" "public" {
   }
 }
 
-resource "aws_instance" "web" {
-  count = 2
-
+resource "aws_instance" "public" {
   ami                         = data.aws_ami.amazonlinux.id
   instance_type               = "t3.micro"
-  subnet_id                   = data.terraform_remote_state.level1.outputs.public_subnet_id[count.index]
+  subnet_id                   = data.terraform_remote_state.level1.outputs.public_subnet_id[0]
   vpc_security_group_ids      = [aws_security_group.public.id]
-  key_name                    = "main"  
+  key_name                    = "main"
   associate_public_ip_address = true
-  user_data                   = file("user-data.sh") 
 
   tags = {
     Name = "${var.env_code}-public"
@@ -69,18 +58,26 @@ resource "aws_security_group" "private" {
   vpc_id      = data.terraform_remote_state.level1.outputs.vpc_id
 
   ingress {
-    description      = "SSH from VPC"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = [data.terraform_remote_state.level1.outputs.vpc_cidr]
+    description     = "HTTP from laod balancer"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.load-balancer.id] 
+  }
+
+  ingress {
+    description = "SSH from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [data.terraform_remote_state.level1.outputs.vpc_cidr]
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
@@ -89,11 +86,14 @@ resource "aws_security_group" "private" {
 }
 
 resource "aws_instance" "private" {
-  ami                         = data.aws_ami.amazonlinux.id
-  instance_type               = "t3.micro"
-  subnet_id                   = data.terraform_remote_state.level1.outputs.private_subnet_id[0]
-  vpc_security_group_ids      = [aws_security_group.private.id]
-  key_name                    = "main"
+  count = 2
+
+  ami                    = data.aws_ami.amazonlinux.id
+  instance_type          = "t3.micro"
+  subnet_id              = data.terraform_remote_state.level1.outputs.private_subnet_id[count.index]
+  vpc_security_group_ids = [aws_security_group.private.id]
+  key_name               = "main"
+  user_data              = file("user-data.sh")
 
   tags = {
     Name = "${var.env_code}-private"
