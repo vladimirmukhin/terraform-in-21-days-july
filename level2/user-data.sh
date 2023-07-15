2657a610522525ab205b7d1503820c420ba98d6d
Vladimir Mukhin <39741635+vladimirmukhin@users.noreply.github.com>
2023-07-15 12:59:34 -0400

    Configure load balancer (#14)

--- level2/user-data.sh
+++ level2/user-data.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 yum update -y
-yum install -y httpd git
-git clone https://github.com/gabrielecirulli/2048.git
-cp -R 2048/* /var/www/html/
+yum install -y httpd 
+echo "Hello from $(hostname)" > /var/www/html/index.html
 systemctl start httpd && systemctl enable httpd
