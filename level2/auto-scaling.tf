866db7e0fdc90ee6a173cac72066084558df1075
Vladimir Mukhin <39741635+vladimirmukhin@users.noreply.github.com>
2023-07-16 12:44:47 -0400

    Configure ssm agent (#17)

--- level2/auto-scaling.tf
+++ level2/auto-scaling.tf
@@ -1,10 +1,51 @@
+data "aws_ami" "amazonlinux" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["137112412989"]
+}
+
+resource "aws_security_group" "private" {
+  name        = "${var.env_code}-private"
+  description = "Private security group"
+  vpc_id      = data.terraform_remote_state.level1.outputs.vpc_id
+
+  ingress {
+    description     = "HTTP from laod balancer"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.load-balancer.id] 
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.env_code}-private"
+  }
+}
+
 resource "aws_launch_configuration" "main" {
-  name_prefix     = "${var.env_code}-"
-  image_id        = data.aws_ami.amazonlinux.id
-  instance_type   = "t3.micro"
-  security_groups = [aws_security_group.private.id]
-  user_data       = file("user-data.sh")
-  key_name        = "main" 
+  name_prefix          = "${var.env_code}-"
+  image_id             = data.aws_ami.amazonlinux.id
+  instance_type        = "t3.micro"
+  security_groups      = [aws_security_group.private.id]
+  user_data            = file("user-data.sh")
+  iam_instance_profile = aws_iam_instance_profile.main.name
 }
 
 resource "aws_autoscaling_group" "main" {
