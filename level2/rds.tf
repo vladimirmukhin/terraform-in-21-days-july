7d9935f80951fd1a9b3078569cb10f3645a78e46
Vladimir <awsvlad@gmail.com>
2023-07-23 12:47:36 -0400

    Remove unneeded locals

--- level2/rds.tf
+++ level2/rds.tf
@@ -11,8 +11,8 @@ module "rds" {
 
   subnet_ids            = data.terraform_remote_state.level1.outputs.private_subnet_id
   env_code              = var.env_code
-  rds_password          = jsondecode(data.aws_secretsmanager_secret_version.rds_password.secret_string)["username"]
-  rds_username          = jsondecode(data.aws_secretsmanager_secret_version.rds_password.secret_string)["passwrod"]
+  rds_password          = jsondecode(data.aws_secretsmanager_secret_version.rds_password.secret_string)["passwrod"]
+  rds_username          = jsondecode(data.aws_secretsmanager_secret_version.rds_password.secret_string)["username"]
   vpc_id                = data.terraform_remote_state.level1.outputs.vpc_id
   source_security_group = module.auto-scaling.security_group_id
 }
